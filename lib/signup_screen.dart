84e97dbc67278479e071bd7774a14dc23744a672
Alllaa <sayedalaa447@gmail.com>
2019-12-16 17:51:20 +0200

    Login screen

---
+++ lib/signup_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static final String id = "signup_screen";
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:premiercoach/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    toAnotherScreen();
+
+  }
+  Future<Timer>toAnotherScreen()async
+  {
+    return new Timer(Duration(seconds: 5),onDoneFinishTime);
+  }
+  onDoneFinishTime()async
+  {
+    Navigator.of(context).pushReplacementNamed(LoginScreen.id);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Color(0xffE90052));
+    return Scaffold(
+      backgroundColor: Color(0xffE90052) ,
+      body: Center(
+        child: Container(
+
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/Premier-League-Transparent-Background 1.png')
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
